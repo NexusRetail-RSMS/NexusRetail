8ca45a2cd844a68de61bfd8f227f39c0cc58e3ea
Aryavansh <aryavansh.120@gmail.com>
2026-06-23 10:18:57 +0530

    Initial Commit

---
+++ NexusRetail/NexusRetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusRetailApp.swift
+//  NexusRetail
+//
+//  Created by Aryavansh on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct NexusRetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
