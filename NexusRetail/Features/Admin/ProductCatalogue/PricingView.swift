398662fb4f3f21f0e2addd840eab6be9ce01b909
Mahak-Sharma <mahaksharma0227@gmail.com>
2026-06-26 17:44:19 +0530

    updating add and edit product features in app

--- NexusRetail/Features/Admin/ProductCatalogue/PricingView.swift
+++
@@ -1,6 +0,0 @@
-//
-//  PricingView.swift
-//  NexusRetail
-//
-//  Created by Mahak on 25/06/26.
-//
--- NexusRetail/Features/Admin/ProductCatalogue/ProductListView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  ProductList.swift
-//  NexusRetail
-//
-//  Created by Mahak on 25/06/26.
-//
-
